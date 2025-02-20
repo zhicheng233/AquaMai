c78c7d744369e6d2dc01a68ce644bf9381381b68
Clansty <i@gao4.pw>
2025-02-20 16:49:59 +0800

    [+] Show version in test menu

---
+++ AquaMai.Mods/UX/TestModeHook.cs
@@ -0,0 +1,18 @@
+﻿using AquaMai.Config.Attributes;
+using AquaMai.Core;
+using DB;
+using HarmonyLib;
+
+namespace AquaMai.Mods.UX;
+
+[ConfigSection(defaultOn: true, exampleHidden: true)]
+public static class TestModeHook
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(TestmodeRootTableRecord), MethodType.Constructor, typeof(int), typeof(string), typeof(string), typeof(string))]
+    public static void ShowAquaMaiVersion(int EnumValue, ref string ___Name)
+    {
+        if (EnumValue != 0) return;
+        ___Name += $"\n<size=90%>AquaMai {BuildInfo.GitVersion}";
+    }
+}
