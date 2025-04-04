e33b28786592998e5fd6a911fad7e8e543e1ff51
Clansty <i@gao4.pw>
2025-04-04 19:13:12 +0800

    [O] GameProcess Skip button use native way

--- AquaMai.Mods/UX/QuickEndPlay.cs
+++ AquaMai.Mods/UX/QuickEndPlay.cs
@@ -1,11 +1,10 @@
 ﻿using AquaMai.Config.Attributes;
-using AquaMai.Core.Helpers;
-using AquaMai.Core.Resources;
 using HarmonyLib;
 using MAI2.Util;
 using Manager;
 using Monitor;
 using Process;
+using UI;
 using UnityEngine;
 
 namespace AquaMai.Mods.UX;
@@ -16,13 +15,20 @@ namespace AquaMai.Mods.UX;
 public class QuickEndPlay
 {
     private static int _timer;
+    private static QuickEndPlayButtonController[] ui = new QuickEndPlayButtonController[2];
 
     [HarmonyPatch(typeof(GameProcess), "OnStart")]
     [HarmonyPostfix]
     public static void GameProcessPostStart(GameMonitor[] ____monitors)
     {
         _timer = 0;
-        ____monitors[0].gameObject.AddComponent<Ui>();
+        for (int i = 0; i < 2; i++)
+        {
+            var main = Traverse.Create(____monitors[i]).Field<CanvasGroup>("Main").Value;
+            var uiButtons = new GameObject();
+            uiButtons.transform.SetParent(main.transform, false);
+            ui[i] = uiButtons.AddComponent<QuickEndPlayButtonController>();
+        }
     }
 
     [HarmonyPatch(typeof(GameProcess), "OnUpdate")]
@@ -36,6 +42,14 @@ public class QuickEndPlay
                 break;
             case > 4:
                 _timer++;
+                if (_timer == 60)
+                {
+                    for (int i = 0; i < 2; i++)
+                    {
+                        ui[i].Initialize(i);
+                    }
+                }
+
                 break;
             default:
                 _timer = 0;
@@ -51,31 +65,35 @@ public class QuickEndPlay
         }
     }
 
-    private class Ui : MonoBehaviour
+    private class QuickEndPlayButtonController : ButtonControllerBase
     {
-        //button position(x,y) topleft point
-        public float topleftx = Screen.width / 2 - GuiSizes.PlayerWidth * 0.097f;
-        public float toplefty = Screen.height - GuiSizes.PlayerWidth * 0.09f;
-        //button size(w,h)
-        public float width = GuiSizes.PlayerWidth * .50f;
-        public float height = GuiSizes.PlayerWidth * .23f;
-
-        public void OnGUI()
+        public override void Initialize(int monitorIndex)
         {
-            if (_timer < 60) return;
+            InitPositions();
+            base.Initialize(monitorIndex);
+            var param = GetFlatButtonParam(FlatButtonType.Skip);
+            CommonButtons = new CommonButtonObject[_positions.Length];
+            CommonButtons[3] = Instantiate(CommonPrefab.GetFlatButtonObject(), _positions[3]);
+            CommonButtons[3].Initialize(MonitorIndex, InputManager.ButtonSetting.Button04, param.LedColor);
+            CommonButtons[3].SetSymbol(param.Image, isFlip: false);
+            CommonButtons[3].SetSE(param.Cue);
+            SetVisible(true, 3);
+        }
 
-            // get button texture 4 -> SKIP
-            Texture2D buttonTexture = ButtonControllerBase.GetFlatButtonParam(4).Image.texture;
-            // set button background to transparent
-            GUIStyle buttonBG = new GUIStyle(GUI.skin.button);
-            buttonBG.normal.background = null;
-            buttonBG.hover.background = null;
-            buttonBG.active.background = null;
-            // rotate -22.5°
-            Vector2 topLeftPosition = new Vector2(topleftx, toplefty);
-            GUIUtility.RotateAroundPivot((float)-22.5, topLeftPosition);
-            // draw button
-            GUI.Button(new Rect(topleftx, toplefty, width, height), buttonTexture, buttonBG);
+        private void InitPositions()
+        {
+            _positions = new Transform[8];
+            var btn4 = new GameObject();
+            var rectTransform = btn4.AddComponent<RectTransform>();
+            btn4.transform.SetParent(transform, false);
+            rectTransform.anchorMin = new Vector2(0.5f, 0.5f);
+            rectTransform.anchorMax = new Vector2(0.5f, 0.5f);
+            rectTransform.pivot = new Vector2(0.5f, 0);
+            rectTransform.anchoredPosition = new Vector2(223, -563);
+            rectTransform.sizeDelta = new Vector2(351, 212);
+            rectTransform.rotation = Quaternion.Euler(0, 0, 22.5f);
+            rectTransform.localScale = new Vector3(1, 1, 1);
+            _positions[3] = rectTransform;
         }
     }
 }
