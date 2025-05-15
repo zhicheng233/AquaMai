671bd724beff224c990fdb0274854535a88c4941
Clansty <i@gao4.pw>
2025-05-15 08:56:15 +0800

    [F] OneKeyRetrySkip crash when both press (real fix)

--- AquaMai.Mods/UX/OneKeyRetrySkip.cs
+++ AquaMai.Mods/UX/OneKeyRetrySkip.cs
@@ -4,6 +4,7 @@ using AquaMai.Core.Helpers;
 using HarmonyLib;
 using MAI2.Util;
 using Manager;
+using MelonLoader;
 using Process;
 
 namespace AquaMai.Mods.UX;
@@ -25,12 +26,30 @@ public class OneKeyRetrySkip
     [ConfigEntry]
     public static readonly bool skipLongPress = true;
 
+    private static bool dirty = false;
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(GameProcess), "OnStart")]
+    public static void PostGameProcessStart()
+    {
+#if DEBUG
+        MelonLogger.Msg("[OneKeyRetrySkip] Dirty flag reset");
+#endif
+        dirty = false;
+    }
+
     [HarmonyPostfix]
     [HarmonyPatch(typeof(GameProcess), "OnUpdate")]
     public static void PostGameProcessUpdate(GameProcess __instance, Message[] ____message, ProcessDataContainer ___container)
     {
+        if (dirty) return;
+
         if (KeyListener.GetKeyDownOrLongPress(skipKey, skipLongPress))
         {
+#if DEBUG
+            MelonLogger.Msg("[OneKeyRetrySkip] Skip key pressed.");
+#endif
+            dirty = true;
             var traverse = Traverse.Create(__instance);
             ___container.processManager.SendMessage(____message[0]);
             Singleton<GamePlayManager>.Instance.SetSyncResult(0);
@@ -39,6 +58,10 @@ public class OneKeyRetrySkip
 
         else if (KeyListener.GetKeyDownOrLongPress(retryKey, retryLongPress) && GameInfo.GameVersion >= 23000)
         {
+#if DEBUG
+            MelonLogger.Msg("[OneKeyRetrySkip] Retry key pressed.");
+#endif
+            dirty = true;
             // This is original typo in Assembly-CSharp
             Singleton<GamePlayManager>.Instance.SetQuickRetryFrag(flag: true);
         }
